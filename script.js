0f2a14798d976902883b87b46ee24400563e5d4e
tinuh <tinuvanapamula@gmail.com>
2020-05-27 16:26:22 -0400

    basic movement

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 let showinstructions = () => $('#instructions').modal('show');
+var forward = true;
+var leftbar = window.document.getElementById('leftbar');
+var pebble = window.document.getElementById('pebble');
+var rightbar = window.document.getElementById('rightbar');
+var interval;
 
 function init(){
      $('#instructions').modal('hide');
+     interval = setInterval(() => move(), 50);
+}
+
+function stop(){
+     clearInterval(interval);
+}
+
+function move(){
+     if (forward){
+          leftbar.style.width = String(parseInt(leftbar.style.width.slice(0, -1)) + 1) + "%";
+          rightbar.style.width = String(parseInt(rightbar.style.width.slice(0, -1)) - 1) + "%";
+          if (parseInt(leftbar.style.width.slice(0, -1)) === 98){
+               forward = false;
+          }
+     }
+     else{
+          leftbar.style.width = String(parseInt(leftbar.style.width.slice(0, -1)) - 1) + "%";
+          rightbar.style.width = String(parseInt(rightbar.style.width.slice(0, -1)) + 1) + "%";
+          if (parseInt(rightbar.style.width.slice(0, -1)) === 98){
+               forward = true;
+          }
+     }
 }
